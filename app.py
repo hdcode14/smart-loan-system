1c9540aa702639b291dc9bd50e38c2fb20f10f44
hdcode14 <hrisihd18@gmail.com>
2025-11-13 12:06:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -665,5 +665,5 @@ def make_admin(user_id):
     return redirect(url_for('admin_dashboard'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
